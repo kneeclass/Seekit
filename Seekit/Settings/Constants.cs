edc85b1b559d69bd6ee09a324f95e12ad1768443
Niklas Emevi <niklasm@meridium.se>
2013-10-18 19:59:26 +0200

    changed prefix format from seekit. to seekit_ this makes null values work in ravendb... strange shit

--- Seekit/Settings/Constants.cs
+++ Seekit/Settings/Constants.cs
@@ -1,7 +1,7 @@
 ﻿namespace Seekit.Settings {
     public class Constants {
         public static string SeekitCrawlerUserAgent = "seekit crawler";
-        public static string SeekitMetaTagFormat = @"<meta name=""seekit.{0}"" content=""{1}"" data-type=""{2}"" data-facet=""{3}"" />";
+        public static string SeekitMetaTagFormat = @"<meta name=""seekit_{0}"" content=""{1}"" data-type=""{2}"" data-facet=""{3}"" />";
         public static string SeekitDefaultDateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK";
         public static string SeekitTypedModelsName = "TypedModels";
     }

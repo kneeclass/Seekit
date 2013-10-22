8285aa73104f76c9bfc2e956fce961b6eb969eb5
Niklas Emevi <niklasm@meridium.se>
2013-10-22 23:05:39 +0200

    Making things look better

--- Seekit/Entities/Query.cs
+++ Seekit/Entities/Query.cs
@@ -6,17 +6,25 @@ using Seekit.Linq;
 namespace Seekit.Entities {
     public class Query {
         public string SearchTerms { get; set; }
+
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Lang { get; set; }
+
         public Int32 Take { get; set; }
+
         public Int32 Skip { get; set; }
+
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string ModelType { get; set; }
+
         public Int32 QueryId { get; set; }
+
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public GeoQuery GeoQuery { get; set; }
+
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public List<ConvertedExpression> Filter { get; set; }
+
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public List<SortOrder> SortOrders { get; set; }
     }
--- Seekit/Linq/ConvertedExpression.cs
+++ Seekit/Linq/ConvertedExpression.cs
@@ -8,13 +8,17 @@ namespace Seekit.Linq {
         public ConvertedExpression() {
             Value = new ExpressionValue();
         }
-
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string Operator { get; set; }
+
         [JsonConverter(typeof(StringEnumConverter))]
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public SubQuery? SubQuery { get; set; }
+
         public string Target { get; set; }
+
         public string Equality { get; set; }
+
         [JsonConverter(typeof(ExpressionValueJsonConverter))]
         public ExpressionValue Value { get; set; }
 
